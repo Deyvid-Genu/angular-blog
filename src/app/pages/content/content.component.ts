55ee466efe6ee0fdd152ea5c781f44b46999e2a6
Deyvid Genu <deyvid.genu@gmail.com>
2023-10-05 16:40:33 -0300

    dynamic content page created

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -7,14 +8,21 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit {
-  newsImage: string =
-    'https://i.cbc.ca/1.6984147.1696196657!/fileImage/httpImage/image.JPG_gen/derivatives/16x9_780/police-arrests-at-street-party.JPG';
-  newsTitle: string = 'NOTÍCIA 1';
-  newsText: string =
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates esse reprehenderit optio sapiente, cum quas fugiat dicta incidunt! Laudantium voluptatem ut tenetur ipsa. Quam a, distinctio ratione laborum blanditiis. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum officiis, ipsa accusamus eligendi delectus architecto excepturi! Veritatis temporibus commodi pariatur. Iure quas vero, ipsa in amet quibusdam sequi hic natus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit omnis totam est, molestiae unde et sapiente mollitia voluptatibus tempore repellendus voluptate iste, quas impedit quos voluptates doloribus quidem, harum eaque.';
+  newsImage: string = '';
+  newsTitle: string = '';
+  newsText: string = '';
+  private id: string | null = '0';
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe((value) => console.log(value.get('id')));
+    this.route.paramMap.subscribe((value) => (this.id = value.get('id')));
+    this.setValuesToComponent(this.id);
+  }
+
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter((article) => article.id === id)[0];
+    this.newsImage = result.image;
+    this.newsTitle = result.title;
+    this.newsText = result.text;
   }
 }
